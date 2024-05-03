087b1bf19a6322ab387ab7b4ba5423f195cb9def
agthm3 <gigam57@gmail.com>
2024-05-03 13:07:51 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InfusionController;
 
 Route::post('/infusions', [InfusionController::class, 'store']);
-
+     
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
